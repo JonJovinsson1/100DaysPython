eaae86b5daae7996054101dc0a4b42fc36d52625
JonJovinsson1 <73032115+JonJovinsson1@users.noreply.github.com>
2022-11-29 20:29:10 +0100

    Update Day4RockPaperScissors.py

--- Day4RockPaperScissors.py
+++ Day4RockPaperScissors.py
@@ -31,13 +31,20 @@ import random
 print("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
 RockPaperScissors = int(input())
 x = random.randint(0,2)
-print(RPS[RockPaperScissors])
-print(RPS[x])
-if RockPaperScissors == x:
+
+if RockPaperScissors < 0 or RockPaperScissors >=3:
+  print("You Lose")
+elif RockPaperScissors == x:
   print("Draw")
+  print(RPS[RockPaperScissors])
+  print(RPS[x])
 elif RockPaperScissors == 0 and x == 1 or RockPaperScissors == 1 and x == 2 or RockPaperScissors == 2 and x == 0:
   print("You Lose")
+  print(RPS[RockPaperScissors])
+  print(RPS[x])
 else:
   print("You Win")
+  print(RPS[RockPaperScissors])
+  print(RPS[x])
 
   
